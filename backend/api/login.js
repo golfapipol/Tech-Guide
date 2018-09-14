0259b483f179e2cb11b101b3e7177c2d7eea0230
Apipol Sukgler <apipol@computerlogy.com>
2018-09-15 01:16:00 +0700

    [Edited]: remove console.log

--- backend/api/login.js
+++ backend/api/login.js
@@ -2,7 +2,6 @@ const LoginService = require('../service/member').Login
 
 const LoginHandler = (request, response, next) => {
     const {email,password} = request.body
-    console.log("email", email, !email, "password", password, !password)
     if (!email || !password) {
         response.send(400, { response: "please input email and password" })
         return Promise.resolve(next())
